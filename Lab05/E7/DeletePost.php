b8cb74a015f16ac861f03f7b147b6a1e8356d096
Afnan Latif <47468307+aLatif01@users.noreply.github.com>
2019-10-25 14:44:54 -0500

    Add files via upload

---
+++ Lab05/E7/DeletePost.php
@@ -0,0 +1,27 @@
+<?php
+
+$mysqli =  new mysqli("mysql.eecs.ku.edu", "alatif01", "Eev3seic", "alatif01");
+
+if ($mysqli->connect_errno)
+{
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$delete = $_POST["checkbox"];
+
+foreach($delete as $value)
+{
+  $query = "DELETE FROM Posts WHERE post_id = '$value'";
+  if($mysqli->query($query) === TRUE)
+  {
+    echo "Post id: " .$value. " has been deleted";
+    echo "<br>";
+  }
+  else
+  {
+    echo "ERROR: " .$sql. "<br>" .$mysqli->error;
+  }
+}
+$mysqli->close();
+ ?>
